b4d4803cde0b3927a91c97decb79e4d429907da2
Vassily Checkin <vcheckin@gmail.com>
2019-03-10 19:53:11 -0400

    apply format

--- src/enum_util.h
+++ src/enum_util.h
@@ -1,4 +1,5 @@
 #pragma once
+/// Copyright (c) 2018 Vassily Checkin. See included license file.
 #include <type_traits>
 #include <algorithm>
 
@@ -7,23 +8,23 @@
  * on enum value
  *
  */
-template <typename A>
-struct is_bitmap: public std::false_type{};
+template <typename A> struct is_bitmap : public std::false_type {};
 
 /** bit of synt sugar for expressions like
  *  id == ID1 || id == ID2 || id == ID3
  *  oneOf(id, {ID1, ID2, ID3});
  */
-template <typename /* @TODO `typename` -> `std::equality_comparable` when <concepts> hdr is available*/ T> 
-bool oneOf(const T& a, const std::initializer_list<T>& l) {
-    return std::any_of(l.begin(), l.end(), [&](auto&& e) {return a == e;});
+template <typename T>
+/* @TODO `typename` -> `std::equality_comparable` when <concepts> hdr is available*/
+bool oneOf(const T &a, const std::initializer_list<T> &l)
+{
+    return std::any_of(l.begin(), l.end(), [&](auto &&e) { return a == e; });
 }
 
 /** bool conversion for bitmap enum */
-template<typename Enum>
-constexpr
-typename std::enable_if<is_bitmap<Enum>::value, bool>::type
-isSet(const Enum& a) 
+template <typename Enum>
+constexpr typename std::enable_if<is_bitmap<Enum>::value, bool>::type
+isSet(const Enum &a)
 {
     using ul = typename std::underlying_type<Enum>::type;
     return static_cast<ul>(a) != 0;
@@ -31,50 +32,44 @@ isSet(const Enum& a)
 
 /// enable bitwise operations for enum class type Enum. Must be in the global namespace
 #define ENABLE_BITMAP_OPERATORS(Enum) \
-template <> struct is_bitmap<Enum>: public std::is_enum<Enum>{}; \
+    template <> struct is_bitmap<Enum> : public std::is_enum<Enum> {};
 
-template<typename Enum>
-constexpr
-typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
-operator|(const Enum& a, const Enum& b)
+template <typename Enum>
+constexpr typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
+operator|(const Enum &a, const Enum &b)
 {
     using ul = typename std::underlying_type<Enum>::type;
-    return static_cast<Enum> (static_cast<ul>(a) | static_cast<ul>(b));
+    return static_cast<Enum>(static_cast<ul>(a) | static_cast<ul>(b));
 }
 
-template<typename Enum>
-constexpr
-typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
-operator|=(Enum& a, const Enum& b)
+template <typename Enum>
+constexpr typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
+operator|=(Enum &a, const Enum &b)
 {
     using ul = typename std::underlying_type<Enum>::type;
-    return a = static_cast<Enum> (static_cast<ul>(a) | static_cast<ul>(b));
+    return a = static_cast<Enum>(static_cast<ul>(a) | static_cast<ul>(b));
 }
 
-template<typename Enum>
-constexpr
-typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
-operator&(const Enum& a, const Enum& b)
+template <typename Enum>
+constexpr typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
+operator&(const Enum &a, const Enum &b)
 {
     using ul = typename std::underlying_type<Enum>::type;
-    return static_cast<Enum> (static_cast<ul>(a) & static_cast<ul>(b));
+    return static_cast<Enum>(static_cast<ul>(a) & static_cast<ul>(b));
 }
 
-template<typename Enum>
-constexpr
-typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
-operator&=(Enum& a, const Enum& b)
+template <typename Enum>
+constexpr typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
+operator&=(Enum &a, const Enum &b)
 {
     using ul = typename std::underlying_type<Enum>::type;
-    return a = static_cast<Enum> (static_cast<ul>(a) & static_cast<ul>(b));
+    return a = static_cast<Enum>(static_cast<ul>(a) & static_cast<ul>(b));
 }
 
-template<typename Enum>
+template <typename Enum>
 typename std::enable_if<is_bitmap<Enum>::value, Enum>::type
-operator~(const Enum& a)
+operator~(const Enum &a)
 {
     using ul = typename std::underlying_type<Enum>::type;
     return static_cast<Enum>(~static_cast<ul>(a));
 }
-
-
